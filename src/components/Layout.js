67c1cc30c81c74dff6834688b93dbf50e576de25
Bumhan Yu <bumhan.yu@gmail.com>
2021-03-22 00:01:38 -0400

    remove unnecessary layout props

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import Info from './info';
 import useSiteMetadata from '../static_queries/useSiteMetadata';
 import layoutStyles from '../styles/components/layout.module.scss';
 
-export default function Layout({ page, children }) {
+export default function Layout({ children }) {
   const { title, description } = useSiteMetadata();
   const [showInfo, setShowInfo] = useState(false);
   const toggleInfo = () => setShowInfo(!showInfo);
@@ -13,22 +13,13 @@ export default function Layout({ page, children }) {
     document.querySelector('body').classList.toggle('isDark');
   };
   return (
-    <section
-      className={`${layoutStyles.layout} ${
-        page === 'info' && layoutStyles.info_page
-      }`}
-    >
+    <section className={layoutStyles.layout}>
       <Helmet>
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
       </Helmet>
-      <Header
-        page={page}
-        title={title}
-        toggleInfo={toggleInfo}
-        toggleColor={toggleColor}
-      />
+      <Header title={title} toggleInfo={toggleInfo} toggleColor={toggleColor} />
       <main className={layoutStyles.content}>{children}</main>
       <Info showInfo={showInfo} toggleInfo={toggleInfo} />
     </section>
--- src/pages/404.js
+++ src/pages/404.js
@@ -5,10 +5,10 @@ import notFoundStyles from '../styles/pages/404.module.scss';
 
 export default function NotFound() {
   return (
-    <Layout page="404" bgColor="inherit">
+    <Layout>
       <div className={notFoundStyles.notFound__container}>
         <Link to="/">
-          <h1>Sorry, couldn't find that page.</h1>
+          <p>Sorry, couldn't find that page.</p>
         </Link>
       </div>
     </Layout>
